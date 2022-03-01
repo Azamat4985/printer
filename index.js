6f048130a797f4fc836c7ea07740806fac295978
Azamat <azick1222@gmail.com>
2022-03-01 17:37:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let isExpandedInfo = false;
+
+	const infoBox = document.querySelector('.about__info')
+	const expandInfoBtn = document.getElementById('expandInfoBtn');
+
+
+	expandInfoBtn.addEventListener('click', () => {
+		if(!isExpandedInfo) {
+			infoBox.style.height = '100%';
+			expandInfoBtn.textContent = 'Закрыть';
+			isExpandedInfo = true;
+		} else {
+			infoBox.style.height = '10em';
+			expandInfoBtn.textContent = 'Закрыть';
+			isExpandedInfo = true;
+		}
+	})
+
+	const swiper = new Swiper('.swiper', {
+		// Optional parameters
+		direction: 'horizontal',
+		loop: true,
+	  
+		// If we need pagination
+		pagination: {
+		  el: '.swiper-pagination',
+		},
+	  
+		// Navigation arrows
+		navigation: {
+		  nextEl: '.swiper-button-next',
+		  prevEl: '.swiper-button-prev',
+		},
+	  
+		// And if we need scrollbar
+		scrollbar: {
+		  el: '.swiper-scrollbar',
+		},
+	  });
+})
